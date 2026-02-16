de61a4ed5688d4d2cc6979f5bebeb04383db51a7
Prakrit khwanphatthalung <poom66310@gmail.com>
2026-02-16 09:27:49 +0700

    test p01

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,12 @@
-export class Student {}
+export class Student {
+    firstname? : string;
+    lastname? :string;
+
+    printName(name : string,last : string): void {
+
+        if(name) {
+            console.log(`${name} ${last}`)
+        }
+    }
+
+}
