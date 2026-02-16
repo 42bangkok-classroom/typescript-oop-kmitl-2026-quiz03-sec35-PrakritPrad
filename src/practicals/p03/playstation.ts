0d878950708fc27b19fcdd775369e84ab76467a5
Prakrit khwanphatthalung <poom66310@gmail.com>
2026-02-16 10:12:03 +0700

    testp03

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -1,5 +1,24 @@
 import { Product } from './product'
 
 export class Playstation extends Product {
-  // TODO: implement class properties, constructor with super(...), and methods
+  readonly name?: string;
+  price: number = 0;
+  generation?: number;
+  constructor(name: string, generation: number, price: number) {
+    super(name)
+  }
+  getProfile() {
+    return `${this.name} (gen ${this.generation})`
+  }
+  getPrice() {
+    return this.price;
+  }
+  setPrice(n : number) {
+    this.price = n;
+  }
+  getDiscountPrice() {
+        const discount = this.price * (Product.DISCOUNT_PERCENT / 100);
+        this.price = this.price - discount;
+    }
+
 }
--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -1,2 +1,21 @@
-export class Product {}
+export class Product {
+    readonly name?: string;
+    price?: number = 0;
+    static readonly DISCOUNT_PERCENT: number = 10;
+    constructor(name: string) {
+        this.name = name;
+
+    }
+    getPrice() {
+        return this.price;
+    }
+    setPrice(price: number) {
+        this.price = price;
+
+    }
+    getDiscountPrice() {
+        const discount = this.price * (percent / 100);
+        this.price = this.price - discount;
+    }
+}
 
