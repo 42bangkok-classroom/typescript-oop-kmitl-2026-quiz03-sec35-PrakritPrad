564aec1fd03ab6d557f2885986644bdfdcba4ff0
PrakritPrad <poom66310@gmail.com>
2026-02-20 19:33:13 +0700

    testset

--- src/practicals/p04/user.ts
+++ src/practicals/p04/user.ts
@@ -1,1 +1,21 @@
-export class User {}
+export class User {
+    public username: string;
+    private password: string;
+    static LOGIN_ATTEMPTS: number = 0 ;
+    constructor(username: string, password: string) {
+        this.username = username;
+        this.password = password;
+    }
+    login(passwrod: string): boolean {
+        if (this.password === passwrod) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    getLoginAttempts() {
+        return User.LOGIN_ATTEMPTS ++;
+    }
+
+}
