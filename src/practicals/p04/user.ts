edffa3e6f938dc2e64c6e03e2015accd67f5faea
PrakritPrad <poom66310@gmail.com>
2026-02-20 20:26:57 +0700

    asdasdkajflksjjdd

--- src/practicals/p04/user.ts
+++ src/practicals/p04/user.ts
@@ -1,23 +1,23 @@
 export class User {
     public username: string;
     private password: string;
-    private LOGIN_ATTEMPTS: number = 0 ;
+    private LOGIN_ATTEMPTS: number = 0;
     constructor(username: string, password: string) {
         this.username = username;
         this.password = password;
     }
     login(password: string): boolean {
         if (this.password === password) {
-
+            this.LOGIN_ATTEMPTS++;
             return true
         }
         else {
- ;
+            this.LOGIN_ATTEMPTS++;
             return false
         }
     }
     getLoginAttempts() {
-        return this.LOGIN_ATTEMPTS++  ;
+        return this.LOGIN_ATTEMPTS;
     }
 
 }
